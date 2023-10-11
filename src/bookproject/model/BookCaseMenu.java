1e635269b4621fd63890f20a1a8320775f97d7f5
yulindratita17 <yulindratita@gmail.com>
2023-10-11 22:57:39 +0700

    add validation input main menu

--- src/bookproject/model/BookCaseMenu.java
+++ src/bookproject/model/BookCaseMenu.java
@@ -11,15 +11,42 @@ public class BookCaseMenu {
         BookRepository bookRepo = new BookRepository();
         // ambil semua data book dari repository
         List<ComercialBook> listAllBook = bookRepo.getAllBook();
-        
+
         String[] mainPublisher = { "Production Cost Termahal", "Production Cost Termurah", "Back To Main Menu" };
         mainMenu();
 
     }
 
-    public static void mainMenu(){
+    public static void mainMenu() {
         String[] mainMenu = { "Book", "Author", "Publisher", "Exit" };
+        int optionMainMenu;
+        boolean isLooping = true;
+
         PrintData.printMenu("Main Menu", mainMenu);
+
+        optionMainMenu = Integer
+                .valueOf(Validation.validateNumberWithRange("Choose item: ", "Only number accepted", Validation.regexNumber, 0, 3));
+
+        do {
+            switch (optionMainMenu) {
+                case 1:
+                    // panggil menu book
+                    break;
+                case 2:
+                    // panggil menu author
+                    break;
+                case 3:
+                    // panggil menu publisher
+                    break;
+                case 0:
+                    // menu exit
+                    isLooping = false;
+
+                    break;
+
+            }
+        } while (isLooping);
+
     }
 
 }
